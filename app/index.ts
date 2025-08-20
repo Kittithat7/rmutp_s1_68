4deeca4170d9eaeeffdbdaac29e8bfd077e63064
kittithat <kittithat-kh@rmutp.ac.th>
2025-08-20 17:13:27 +0700

    install hono/node-server packege

--- app/index.ts
+++ app/index.ts
@@ -2,5 +2,9 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get('/', (c) => c.text("Hello,World!"));
-app.get('about',(c) => {return c.json({mesage: "About Page"})});
+app.get('about',(c) => {
+    return c.json({mesage: "About Page"
+    })
+});
+
 export default app
