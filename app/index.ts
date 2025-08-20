d263ec1773aaa8c60c062b1b4cf8ed449f6790cb
kittithat <kittithat-kh@rmutp.ac.th>
2025-08-20 16:58:51 +0700

    Create app

---
+++ app/index.ts
@@ -0,0 +1,6 @@
+import { Hono } from "hono";
+const app = new Hono();
+
+app.get('/', (c) => c.text("Hello,World!"));
+app.get('about',(c) => {return c.json({mesage: "About Page"})});
+export default app
