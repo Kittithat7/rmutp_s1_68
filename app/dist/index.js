7f0f12ea91d140940f5eefbab5db7bd701faebad
kittithat <kittithat-kh@rmutp.ac.th>
2025-08-27 14:31:16 +0700

    update run tsc

--- app/dist/index.js
+++ app/dist/index.js
@@ -3,4 +3,5 @@ const app = new Hono();
 app.get('/', (c) => {
     return c.text('Hello from Hono!');
 });
+app.get('/about', (c) => { return c.json({ message: "Kittithat khunklangsaeng" }); });
 export default app;
--- app/index.ts
+++ app/index.ts
@@ -5,5 +5,5 @@ const app = new Hono();
 app.get('/', (c) => {
   return c.text('Hello from Hono!');
 });
-
+app.get('/about', (c) => {return c.json({message: "Kittithat khunklangsaeng"})});
 export default app;
