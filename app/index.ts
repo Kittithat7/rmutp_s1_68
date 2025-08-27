70964d7548fb4879a114168815e704d63f479a6d
kittithat <kittithat-kh@rmutp.ac.th>
2025-08-27 15:42:38 +0700

    add app

--- app/index.ts
+++ app/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.get('/', (c) => {
+app.get('/about', (c) => {
   return c.text('Hello from Hono!');
 });
 app.get('/about', (c) => {return c.json({message: "Kittithat khunklangsaeng"})});
