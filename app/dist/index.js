075fdb3109052dd0d05b4f70e6aa3c9a17fd20e8
kittithat <kittithat-kh@rmutp.ac.th>
2025-08-27 15:45:44 +0700

    delete

--- app/dist/index.js
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono';
-const app = new Hono();
-app.get('/', (c) => {
-    return c.text('Hello from Hono!');
-});
-app.get('/about', (c) => { return c.json({ message: "Kittithat khunklangsaeng" }); });
-export default app;
--- app/dist/server.js
+++
@@ -1,7 +0,0 @@
-import { serve } from '@hono/node-server';
-import app from './index.js';
-serve({
-    fetch: app.fetch,
-    port: 3000,
-});
-console.log('🚀 Server is running at http://localhost:3000');
