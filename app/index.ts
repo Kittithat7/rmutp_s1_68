6c352c6c74ec882d4e1e1add8764dfc038886486
kittithat <kittithat-kh@rmutp.ac.th>
2025-09-03 15:47:30 +0700

    update app_post

--- app/index.ts
+++ app/index.ts
@@ -13,4 +13,14 @@ app.get("/profile", async(c) => {
     return c.json(profile);
 });
 
+app.post("/profile", async(c) => {
+    //logic to create a profile
+    const body = await c.req.json();
+    console.log('input of profile ', body);
+    
+    return c.json({
+        message: "create profile completed"
+    });
+});
+
 export default app;
